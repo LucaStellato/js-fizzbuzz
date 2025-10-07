ca1089a1cb07c945ae11dc6052d5cabcded8fdee
LucaStellato <lucastellato435@gmail.com>
2025-10-07 13:49:04 +0200

    add comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,18 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 
-
+// Creiamo un ciclo for che gira fin quando la nostra variabile i non si incrementa fino ad arrivare al numero 100
 for(let i=1; i<=100; i++)
 {
-    if(i % 3 === 0){
-        console.log(`${i} Fizz`);
+    // verifichiamo SE la nostra variabile incrementale é divisibile sia per 5 che per 3 
+    if( i % 5 === 0 && i % 3 === 0){
+        console.log(`${i} FizzBuzz`);
         
     }else if( i % 5 === 0){
-         console.log("Buzz");
+         console.log(`${i} Buzz`);
     }else{
-        if( i % 5 === 0 && i % 3 === 0){
-            console.log("FizzBuzz");
+        if( i % 3 === 0){
+            console.log(`${i} Fizz`);
         }
     }
 }
